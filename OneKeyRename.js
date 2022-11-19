c2db43291f5a24cc3fb6526ee5349291dbc0aefe
dzc980812 <980861111@qq.com>
2022-11-19 19:26:28 +0800

    feat:增加一键改名及复制前缀功能

--- OneKeyRename.js
+++ OneKeyRename.js
@@ -4,7 +4,7 @@
 // @description  2022.11.14
 // @author       Zccc
 // @namespace    115ReName@Zccc
-// @version      0.0.3
+// @version      0.0.4
 // @match        https://115.com/*
 // @exclude      https://115.com/s/*
 
@@ -13,7 +13,7 @@
 // @connect      webapi.115.com
 // @connect      115.com
 
-
+// @grant        GM_setClipboard
 // ==/UserScript==
 
 'use strict';
@@ -55,9 +55,9 @@
   }
 
   // 添加dom及点击事件
-  function addDomClick(dom, toopTip, pItem, rename) {
+  function addDomClick(dom, toolTip, pItem, rename) {
 
-    dom.prependTo(toopTip);
+    dom.prependTo(toolTip);
 
     dom[0].addEventListener('click', async (e) => {
       fetchReName(pItem.id, rename)
@@ -65,57 +65,155 @@
   }
 
   // 删除 @ 之前的
-  function removeEmail(toopTip, pItem) {
+  function removeEmail(toolTip, pItem) {
     var $btn = $('<a ><i></i><div style="background:white"><span>删除@</span></div></a>');
 
     const reName = (pItem.name.slice(pItem.name.indexOf('@') + 1));
 
-    addDomClick($btn, toopTip, pItem, reName)
+    addDomClick($btn, toolTip, pItem, reName)
   }
 
   // 删除 ] 之前的 部分名称前缀会以[]开头
-  function removeBracketsR(toopTip, pItem) {
+  function removeBracketsR(toolTip, pItem) {
     var $btn = $('<a ><i></i><div style="background:white"><span>删除]</span></div></a>');
     const reName = (pItem.name.slice(pItem.name.indexOf(']') + 1));
 
-    addDomClick($btn, toopTip, pItem, reName)
+    addDomClick($btn, toolTip, pItem, reName)
   }
 
 
   // 删除 - 之前的
-  function removeBar(toopTip, pItem) {
+  function removeBar(toolTip, pItem) {
     var $btn = $('<a ><i></i><div style="background:white"><span>删除-</span></div></a>');
     const reName = (pItem.name.slice(pItem.name.indexOf('-') + 1));
 
-    addDomClick($btn, toopTip, pItem, reName)
+    addDomClick($btn, toolTip, pItem, reName)
   }
 
   // 删除 最后一个 - 之后的内容
-  function removeLastBar(toopTip, pItem) {
+  function removeLastBar(toolTip, pItem) {
     console.log(pItem, 'pItempItem')
     var $btn = $('<a ><i></i><div style="background:white"><span>删除尾-</span></div></a>');
     const reName = (pItem.name.slice(0, pItem.name.lastIndexOf('-')));
 
-    addDomClick($btn, toopTip, pItem, reName)
+    addDomClick($btn, toolTip, pItem, reName)
   }
 
   // 转大写
-  function nameToUpper(toopTip, pItem) {
+  function nameToUpper(toolTip, pItem) {
     var $btn = $('<a ><div style="background:white" class="rename" rename="up"><span>转大</span></div></a>');
     const reName = pItem.name.toLocaleUpperCase('en-US')
 
-    addDomClick($btn, toopTip, pItem, reName)
+    addDomClick($btn, toolTip, pItem, reName)
   }
 
+  // 复制前缀
+  function copyFrontPlacket(toolTip, pItem) {
+    var $btn = $('<a ><div style="background:white" class="rename" rename="up"><span>复制前缀</span></div></a>');
+    const reName = (pItem.name.slice(0, pItem.name.indexOf('-')));
+
+    $btn.prependTo(toolTip);
+
+    $btn[0].addEventListener('click', async (e) => {
+      GM_setClipboard(reName);
+    })
+  }
+
+  // 获取番号
+  function getVideoCode(title) {
+    title = title.toUpperCase().replace("SIS001", "")
+      .replace("1080P", "")
+      .replace("720P", "");
+
+    let t = title.match(/T28[\-_]\d{3,4}/);
+    // 一本道
+    if (!t) {
+      t = title.match(/1PONDO[\-_]\d{6}[\-_]\d{2,4}/);
+      if (t) {
+        t = t.toString().replace("1PONDO_", "")
+          .replace("1PONDO-", "");
+      }
+    }
+    if (!t) {
+      t = title.match(/HEYZO[\-_]?\d{4}/);
+    }
+    if (!t) {
+      // 加勒比
+      t = title.match(/CARIB[\-_]\d{6}[\-_]\d{3}/);
+      if (t) {
+        t = t.toString().replace("CARIB-", "")
+          .replace("CARIB_", "");
+      }
+    }
+    if (!t) {
+      // 东京热
+      t = title.match(/N[-_]\d{4}/);
+    }
+    if (!t) {
+      // Jukujo-Club | 熟女俱乐部
+      t = title.match(/JUKUJO[-_]\d{4}/);
+    }
+    // 通用
+    if (!t) {
+      t = title.match(/\d+[A-Za-z]+[-_]?\d+/);
+    }
+    if (!t) {
+      t = title.match(/[A-Z]{2,5}[-_]\d{3,5}/);
+    }
+    if (!t) {
+      t = title.match(/\d{6}[\-_]\d{2,4}/);
+    }
+    if (!t) {
+      t = title.match(/[A-Z]+\d{3,5}/);
+    }
+    if (!t) {
+      t = title.match(/[A-Za-z]+[-_]?\d+/);
+    }
+    if (!t) {
+      t = title.match(/\d+[-_]?\d+/);
+    }
+    if (!t) {
+      t = title;
+    }
+    if (t) {
+      t = t.toString().replace("_", "-");
+      console.log("找到番号:" + t);
+      return t;
+    }
+  }
+
+  // 一键改名，自动复制前缀
+  function oneKeyReName(toolTip, pItem) {
+    var $btn = $('<a ><div style="background:white" class="rename"><span>一键改名</span></div></a>');
+    let reName = getVideoCode(pItem.name)
+    let regExp = new RegExp(reName + "[_-]C");
+    let match = pItem.name.toUpperCase().match(regExp);
+
+    if (match) {
+      reName = reName + '-C'
+    }
+
+
+    $btn.prependTo(toolTip);
+
+    $btn[0].addEventListener('click', async (e) => {
+      GM_setClipboard(reName.slice(0, reName.indexOf('-')));
+      fetchReName(pItem.id, reName)
+    })
+  }
+
+
   // 新增一个DOM避免与位置错乱
-  function addDom(toopTip, LiItem) {
+  function addDom(toolTip, LiItem) {
     var $dom = $('<div style="width:100%; height:22px;"></div>');
     removeEmail($dom, LiItem)
     removeBar($dom, LiItem)
     nameToUpper($dom, LiItem)
     removeBracketsR($dom, LiItem)
     removeLastBar($dom, LiItem)
-    $dom.prependTo(toopTip);
+    copyFrontPlacket($dom, LiItem)
+    oneKeyReName($dom, LiItem)
+    $dom.prependTo(toolTip);
   }
 
   // 重命名初始化
